698a5e12479e80cd9429ef7992f141a74aa9aad4
akash-nepak <akashnepak7@outlook.com>
2025-07-09 04:19:11 +0530

    first commmit

---
+++ training/tennis-ball-detection-1/README.dataset.txt
@@ -0,0 +1,6 @@
+# tennis ball detection > 2025-07-02 3:20am
+https://universe.roboflow.com/akashnepak/tennis-ball-detection-jus55
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ yolo_inference.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+model = YOLO('/home/akash-1/TENNIS_ANALYSIS/training/runs/detect/train4/weights/best.pt')
+
+result = model.predict('/home/akash-1/TENNIS_ANALYSIS/input_videos/input_video.mp4',conf = 0.2,save =True)
+
+print(result)
+
+print("boxes:")
+for box in result[0].boxes:
+    print(box)
